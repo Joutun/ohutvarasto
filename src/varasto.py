5ea97d40e48328882b772e55d19891c665824db1
juvekole <jussi.v.kolehmainen@student.jyu.fi>
2025-11-06 00:20:22 +0200

    pylint. Korjattiin koodi, jotta testit läpi

--- src/varasto.py
+++ src/varasto.py
@@ -1,9 +1,7 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo=0):
         if tilavuus > 0.0:
-            if tilavuus > 0.0:
-                if tilavuus > 0.0:
-                    self.tilavuus = tilavuus
+            self.tilavuus = tilavuus
         else:
             # virheellinen, nollataan
             self.tilavuus = 0.0
@@ -18,7 +16,7 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+    #ominaisuus voidaan myös laskea.Ei tarvita erillistä kenttää viela_tilaa tms
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
